32c40fc38d6064b7f13e2b8198a221b6cff0a082
aoyuru <102245971+1Aost@users.noreply.github.com>
2025-05-30 23:30:56 +0800

    feat:create random rgb or hex color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Accordian from './components/accordian'
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     // Accrodian component
-    <Accordian />
+    // <Accordian />
+
+    // Random Color component
+    <RandomColor />
   )
 }
 
