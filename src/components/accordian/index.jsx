e68197a98d92e943b8567e0b488d4352ce9839fe
aoyuru <102245971+1Aost@users.noreply.github.com>
2025-05-30 22:10:47 +0800

    feat:multiple selection

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -7,24 +7,45 @@ import "./styles.css";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
+  // single selection
   const handleSingleSelection = (currentKey) => {
     setSelected(currentKey === selected ? null : currentKey);
   }
 
+  // multi selection
+  const handleMultiSelection = (currentKey) => {
+    let cpyMultiple = [...multiple];
+    let index = cpyMultiple.indexOf(currentKey);
+
+    if (index === -1) cpyMultiple.push(currentKey);
+    else cpyMultiple.splice(index, 1);
+
+    setMultiple(cpyMultiple);
+  }
+
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
       <div className="accrodian">
         {
           data && data.length > 0 ?
             data.map(dataItem => (
               <div className="item" key={dataItem.key}>
-                <div className="title" onClick={() => handleSingleSelection(dataItem.key)}>
+                <div className="title" onClick={
+                  !enableMultiSelection ?
+                    () => handleSingleSelection(dataItem.key) :
+                    () => handleMultiSelection(dataItem.key)}>
                   <p>{dataItem.label}</p>
                   <span>+</span>
                 </div>
                 {
-                  selected === dataItem.key && <div className="content">{dataItem.children}</div>
+                  enableMultiSelection ? (
+                    multiple.indexOf(dataItem.key) !== -1 && <div className="content">{dataItem.children}</div>
+                  ) :
+                    selected === dataItem.key && <div className="content">{dataItem.children}</div>
                 }
               </div>
             ))
